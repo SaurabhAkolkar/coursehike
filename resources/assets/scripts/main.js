fb5c49df1c648a382ddc2e52facd5d6608860a34
Yshvanth <eshva25@gmail.com>
2020-10-02 22:58:11 +0530

    Brought the sass and made it to work.

---
+++ resources/assets/scripts/main.js
@@ -0,0 +1,45 @@
+$(function(){
+    // Global Dropdown Toggle: Start
+    $('.dropdown-toggle').dropdown()
+    // Global Dropdown Toggle: End
+
+    //Swiper Js for Login & Register page
+    var swiper = new Swiper('.entry-swiper-container', {
+        fadeEffect: { crossFade: true },
+        virtualTranslate: true,
+        slideToClickedSlide: true,
+        mousewheelControl: true,
+        autoplayDisableOnInteraction: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        },
+        speed: 1000, 
+        slidersPerView: 1,
+        effect: "fade"
+    });
+
+    //Swiper Js for Artist gallery
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        // spaceBetween: 10,
+        slidesPerView: 13,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+      });
+      var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+          swiper: galleryThumbs
+        }
+      });
+
+}); 
