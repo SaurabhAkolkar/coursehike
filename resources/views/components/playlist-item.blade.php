bc48c050e5dabce63e21cee7ced4efc66e55fffe
Anvesh Nandagiri <49784558+Anvesh0952@users.noreply.github.com>
2020-11-20 18:11:00 +0530

    Update playlist-item.blade.php

--- resources/views/components/playlist-item.blade.php
+++ resources/views/components/playlist-item.blade.php
@@ -1,7 +1,14 @@
 <div class="col-md-3 la-playlist__item">
     <div class="la-playlist__item-top position-relative mb-4">
         <div class="la-playlist__option-more la-playlist__option-more--white position-absolute">
-            <span class="d-inline-block p-2"><i class="la-icon la-icon--2xl icon icon-menu"></i></span>
+            <a class="dropdown-toggle d-inline-block la-course__menubtn p-2" id="playlist_menu" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="la-icon la-icon--2xl icon icon-menu"></i>
+            </a>
+            <div class="la-cmenu dropdown-menu py-0">
+                <a class="dropdown-item la-cmenu__item d-inline-flex"><i class="icon icon-edit la-icon la-icon--lg mr-4"></i>Edit Playlist</a>
+                <a class="dropdown-item la-cmenu__item d-inline-flex"><i class="icon icon-delete la-icon la-icon--lg mr-4"></i>Delete Playlist</a>
+                <a class="dropdown-item la-cmenu__item d-inline-flex" href="/browse/courses"><i class="icon icon-courses la-icon la-icon--lg mr-4"></i>Browse Courses</a>
+            </div>
         </div>
         <div class="la-playlist__thumbnails  la-playlist__thumbnails--three d-flex flex-wrap">
             <div class="la-playlist__thumbnail">
