36da96733f51ccbf520e16cb01cd1be0af36383f
officialvikramsuthar <officialvikramsuthar@gmail.com>
2021-01-19 17:48:42 +0530

    learner count for search and playlist

--- resources/views/components/playlist-item.blade.php
+++ resources/views/components/playlist-item.blade.php
@@ -1,43 +1,41 @@
 
-<div class="col-md-4 la-playlist__item mb-6 mb-md-10 la-anim__stagger-item" role="button">
+<div class="col-md-4 la-playlist__item mb-6 mb-md-10 la-anim__stagger-item" role="button"> 
     <div class="la-playlist__item-top position-relative mb-4">
-        <a class="la-course__inner-link"  href= "{{ '/playlist/'.$id }}" >
-            <div class="la-playlist__option-more la-playlist__option-more--white position-absolute">
-                <span class="dropdown-toggle d-inline-block la-course__menubtn p-2" id="playlist_menu" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="la-icon la-icon--2xl icon icon-menu"></i>
-                </span>
-                <div class="la-cmenu dropdown-menu py-0">
-                    <span class="dropdown-item la-cmenu__item d-inline-flex" onclick="editPlaylistPopup({{$id}}, '{{$courseName}}')"><i class="icon icon-edit la-icon la-icon--lg mr-4"></i>Edit Playlist</span>
-                    <span class="dropdown-item la-cmenu__item d-inline-flex" onclick="location.href='/playlist-delete/{{$id}}'"><i class="icon icon-delete la-icon la-icon--lg mr-4"></i>Delete Playlist</span>
-                    <span class="dropdown-item la-cmenu__item d-inline-flex" onclick="location.href='/playlist/{{$id}}'"><i class="icon icon-courses la-icon la-icon--lg mr-4"></i>See Courses</span>
-                </div>
+        <div class="la-playlist__option-more la-playlist__option-more--white position-absolute">
+            <a class="dropdown-toggle d-inline-block la-course__menubtn p-2" id="playlist_menu" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="la-icon la-icon--2xl icon icon-menu"></i>
+            </a>
+            <div class="la-cmenu dropdown-menu py-0">
+                <a class="dropdown-item la-cmenu__item d-inline-flex" onclick="editPlaylistPopup({{$id}}, '{{$courseName}}')"><i class="icon icon-edit la-icon la-icon--lg mr-4"></i>Edit Playlist</a>
+                <a class="dropdown-item la-cmenu__item d-inline-flex" href="/playlist-delete/{{$id}}"><i class="icon icon-delete la-icon la-icon--lg mr-4"></i>Delete Playlist</a>
+                <a class="dropdown-item la-cmenu__item d-inline-flex" href="/playlist/{{$id}}"><i class="icon icon-courses la-icon la-icon--lg mr-4"></i>See Courses</a>
             </div>
+        </div>
         
-            <div class="la-playlist__thumbnails  la-playlist__thumbnails--three d-flex flex-wrap">
-                @if($classesCount <= 1)    
+        <div class="la-playlist__thumbnails  la-playlist__thumbnails--three d-flex flex-wrap">
+            @if($classesCount <= 1)    
+            <div class="la-playlist__thumbnail">
+                <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
+            </div>
+            @elseif($classesCount == 2)
                 <div class="la-playlist__thumbnail">
                     <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
                 </div>
-                @elseif($classesCount == 2)
-                    <div class="la-playlist__thumbnail">
-                        <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
-                    </div>
-                    <div class="la-playlist__thumbnail">
-                        <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail" width="100%">
-                    </div>
-                @else
-                    <div class="la-playlist__thumbnail">
-                        <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
-                    </div>
-                    <div class="la-playlist__thumbnail">
-                        <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
-                    </div>
-                    <div class="la-playlist__thumbnail">
-                        <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
-                    </div>
-                @endif
-            </div>
-        </a>
+                <div class="la-playlist__thumbnail">
+                    <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail" width="100%">
+                </div>
+            @else
+                <div class="la-playlist__thumbnail">
+                    <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
+                </div>
+                <div class="la-playlist__thumbnail">
+                    <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
+                </div>
+                <div class="la-playlist__thumbnail">
+                    <img class="img-fluid" src="https://picsum.photos/600/500" alt="thumbnail">
+                </div>
+            @endif
+        </div>
         
     </div>
     <div class="la-playlist__item-bottom d-flex justify-content-between">
