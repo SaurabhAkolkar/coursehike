ee6274c422274434c2032c21dfaff0cb0a1fd46d
officialvikramsuthar <73328353+officialvikramsuthar@users.noreply.github.com>
2021-03-10 20:20:13 +0530

    bug fix

--- resources/views/learners/pages/course.blade.php
+++ resources/views/learners/pages/course.blade.php
@@ -264,14 +264,9 @@ $course_id = $course->id;
               <div class="col-lg-12 px-0">
                 <div class="col-12 col-lg px-0 la-anim__wrap">
                   <div class="la-ctabs__about la-anim__stagger-item ">
-                  @php
-                      $discription = $course->short_detail.' '.$course->detail;
-                      $first_string = explode(".", strip_tags($discription), 3);
-                      $length = strlen($first_string[0]) + strlen($first_string[1]) +strlen($first_string[2]);
-                  @endphp
-                    <p>{{ $first_string[0].'. '.$first_string[1].'. '.$first_string[2].'.' }}</p>
+                    <p>{!! $course->short_detail !!}</p>
                     <span class="la-ctabs__about-collapse collapse" id="about_collapse">
-                      {{ substr(strip_tags($discription), $length+3) }}
+                      {!! $course->detail  !!}
                     </span>
                     
                   </div>
