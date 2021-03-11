3d23f54ae6fc06c7457e1dd528e7f29b2e1e7d7e
Anvesh Nandagiri <49784558+Anvesh0952@users.noreply.github.com>
2021-03-11 16:49:44 +0530

    purchase-history mobile

--- resources/views/components/purchase.blade.php
+++ resources/views/components/purchase.blade.php
@@ -1,4 +1,4 @@
-<div class="la-puchaseh__items">
+<div class="la-puchaseh__items d-none d-md-block">
     <div class="la-purchaseh__item row align-items-center pb-8 la-anim__stagger-item">           
         {{-- <div class="col-lg-4 ">
             <div class="la-purchaseh__item-info d-flex align-items-center">
@@ -44,35 +44,36 @@
 
 
   <!-- For Mobile Version-->
-{{--<div class="la-purchaseh__items-mobile d-block d-lg-none">
-    <div class="la-purchaseh__items d-flex justify-content-between">
-        <div class="la-purchaseh__item align-items-center mb-4 ">           
-            <div class="la-purchaseh__item-info d-flex align-items-start">
-            <div class="la-purchaseh__item-date text-sm text-md-md">{{ $invoice }}</div>
+<div class="la-purchaseh__item-mobile d-block d-md-none">
+    <div class="la-purchaseh__item d-flex justify-content-between align-items-start">
+        <div class="col-10 px-0 la-purchaseh__item align-items-center mb-4 ">           
+            <div class="la-purchaseh__item-info la-anim__stagger-item">
+                <div class="la-purchaseh__item-date text-sm text-md-md">{{ $invoice }}</div>
             </div>
         </div>
 
-        <div class="la-purchaseh__item-paystatus la-anim__stagger-item"> {{ $paystatus }}
-            <div class="la-purchaseh__item-toggler collapsed position-relative text-sm mt-16" data-toggle="collapse" data-target="#purchased_{{ $id }}" aria-expanded="false" aria-controls="1">Details</div>
+        <div class="col-2 px-0 la-purchaseh__item-paystatus text-capitalize la-anim__stagger-item"> {{ $paystatus }}
+            <div class="la-purchaseh__item-toggler collapsed position-relative text-sm mt-2 mr-3" data-toggle="collapse" data-target="#purchased_{{ $id }}" aria-expanded="false" aria-controls="1">Details</div>
         </div>
     </div>
     
-    <div class="la-purchaseh__item-content collapse mt-3 mb-8" id="purchased_{{ $id  }}">
+    <div class="la-purchaseh__item-content collapse mt-5 mb-4" id="purchased_{{ $id  }}">
         <div class="la-purchaseh__label-mobile d-flex justify-content-between">
-            <p class="text-md">Purchase Date</p>
+            <p class="text-sm">Purchase Date</p>
             <div class="la-purchaseh__item-date text-md ml-auto">{{ $date }}</div>
         </div>
 
         <div class="la-purchaseh__label-mobile d-flex justify-content-between">
-            <p class="text-md">Paid Amount</p>
+            <p class="text-sm">Paid Amount</p>
             <div class="la-purchaseh__item-total">{{ getSymbol(). $total }}</div>
         </div>
 
         <div class="la-purchaseh__label-mobile d-flex justify-content-between">
-            <a class="la-purchaseh__item-invoice text-xs text-md-md" role="button" target="_blank" href= {{ $invoiceUrl }}>Invoice
+            <p class="text-sm">Download Invoice</p>
+            <a class="la-purchaseh__item-invoice text-md" role="button" target="_blank" href= {{ $invoiceUrl }}>Invoice
                 <span class="la-icon icon-download la-purchaseh__item-download" ></span>
             </a>
         </div>
     </div>
 
-</div> --}}
+</div>
