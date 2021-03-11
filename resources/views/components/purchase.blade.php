919b4bfe7a153924886d06598e137e73d339c715
Anvesh Nandagiri <49784558+Anvesh0952@users.noreply.github.com>
2021-03-11 16:03:57 +0530

    Emailers

--- resources/views/components/purchase.blade.php
+++ resources/views/components/purchase.blade.php
@@ -41,3 +41,38 @@
         </div>
     </div>
   </div>
+
+
+  <!-- For Mobile Version-->
+{{--<div class="la-purchaseh__items-mobile d-block d-lg-none">
+    <div class="la-purchaseh__items d-flex justify-content-between">
+        <div class="la-purchaseh__item align-items-center mb-4 ">           
+            <div class="la-purchaseh__item-info d-flex align-items-start">
+            <div class="la-purchaseh__item-date text-sm text-md-md">{{ $invoice }}</div>
+            </div>
+        </div>
+
+        <div class="la-purchaseh__item-paystatus la-anim__stagger-item"> {{ $paystatus }}
+            <div class="la-purchaseh__item-toggler collapsed position-relative text-sm mt-16" data-toggle="collapse" data-target="#purchased_{{ $id }}" aria-expanded="false" aria-controls="1">Details</div>
+        </div>
+    </div>
+    
+    <div class="la-purchaseh__item-content collapse mt-3 mb-8" id="purchased_{{ $id  }}">
+        <div class="la-purchaseh__label-mobile d-flex justify-content-between">
+            <p class="text-md">Purchase Date</p>
+            <div class="la-purchaseh__item-date text-md ml-auto">{{ $date }}</div>
+        </div>
+
+        <div class="la-purchaseh__label-mobile d-flex justify-content-between">
+            <p class="text-md">Paid Amount</p>
+            <div class="la-purchaseh__item-total">{{ getSymbol(). $total }}</div>
+        </div>
+
+        <div class="la-purchaseh__label-mobile d-flex justify-content-between">
+            <a class="la-purchaseh__item-invoice text-xs text-md-md" role="button" target="_blank" href= {{ $invoiceUrl }}>Invoice
+                <span class="la-icon icon-download la-purchaseh__item-download" ></span>
+            </a>
+        </div>
+    </div>
+
+</div> --}}
