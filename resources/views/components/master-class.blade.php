e1f573deb1dca8d6d5a02c4f3b2aba0cf592d651
Anvesh Nandagiri <49784558+Anvesh0952@users.noreply.github.com>
2021-03-16 11:32:50 +0530

    Icons on Master class

--- resources/views/components/master-class.blade.php
+++ resources/views/components/master-class.blade.php
@@ -11,6 +11,61 @@
          <a class="la-mccourse__type" href="/learn/course/{{$id}}/{{$slug}}">
             <span class="la-mccourse__type-icon la-icon la-icon--md icon-play"></span>
         </a> -->
+
+        <div class="la-mccourse__overlay-info d-flex justify-content-between align-items-start">
+            <ul class="la-course__options la-mccourse__options list-unstyled text-white" id="la-course__nested-links">
+                <li class="la-course__option">
+                    @if(Auth::check())
+                        @if($price && $bought == null)
+                            <span class="d-inline-block la-course__addtocart" onclick="addToCart({{$id}})">
+                                <i class="la-icon la-icon--2xl icon icon-cart @if($checkCart) text-warning @endif"></i>
+                            </span>
+                        @else
+                    @endif
+                    @else
+                        <span class="d-inline-block la-course__addtocart" data-toggle="modal" data-target="#locked_login_modal">
+                            <i class="la-icon la-icon--2xl icon icon-cart"></i>
+                        </span>
+                    @endif
+                </li>
+
+                <li class="la-course__option">
+                    @if(Auth::check())
+                        @if($price && $bought == null)
+                            <span @if($addedToWhishList) onclick="location.href='/remove-from-wishlist/{{$id}}'" @else onclick="addToWishList({{$id}})" @endif  >
+                                <span class="d-inline-block la-course__like">
+                                    <i class="la-icon la-icon--2xl icon @if($checkWishList) text-warning @endif icon-wishlist @if($addedToWhishList) text-warning @endif"></i>
+                                </span>
+                            </span>
+                        @else
+                        @endif
+                    @else
+                        <span data-toggle="modal" data-target="#locked_login_modal">
+                            <span class="d-inline-block la-course__like">
+                                <i class="la-icon la-icon--2xl icon icon-wishlist"></i>
+                            </span>
+                        </span>
+                    @endif
+                </li>
+
+                <li class="la-course__option">
+                    @if(Auth::check())
+                        <span  @if($removeFromPlaylist) onclick="location.href='{{url()->current()}}/{{$id}}'"  @else onclick="showAddToPlaylist({{$id}})" @endif>
+                            <span class="d-inline-block la-course__like">
+                                <i class="la-icon  la-icon--2xl icon-playlist @if($removeFromPlaylist) text-warning  @endif"></i> 
+                            </span>
+                        </span>
+                        @else   
+                        <span data-toggle="modal" data-target="#locked_login_modal">
+                            <span class="d-inline-block la-course__like">
+                                <i class="la-icon la-icon--2xl icon-playlist"></i> 
+                            </span>
+                        </span>
+                    @endif
+                </li>
+            </ul>
+        </div>
+
         <div class="la-mccourse__play position-relative text-center w-100 mt-auto">
             <a  role="button" href="/learn/course/{{$id}}/{{$slug}}" class="la-mccourse__play-btn">
                 <svg class="la-mccourse__play-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"  x="0px" y="0px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
