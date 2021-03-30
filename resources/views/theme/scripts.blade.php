a0bb4fb82375d62cbc8ae85d6ca01b7a8aef2e23
Harish Kumar <harishcanudo@gmail.com>
2021-03-30 15:34:44 +0530

    Google Tag Manager

--- resources/views/theme/scripts.blade.php
+++ resources/views/theme/scripts.blade.php
@@ -42,6 +42,14 @@ $rtl = array('ar','he','ur', 'arc', 'az', 'dv', 'ku'); //make a list of rtl lang
   gtag('config', '{{ $gsetting->google_ana }}');
 </script>
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5CW2KJ5');</script>
+<!-- End Google Tag Manager -->
+
 @if($gsetting->rightclick=='1')
 	<script>
 		(function($) {
