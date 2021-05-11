8c6248f0ff27b952db12cc37c6f0274c068dcfb6
Harish Kumar <harishcanudo@gmail.com>
2021-05-11 16:37:32 +0530

    Update classes.blade.php

--- resources/views/learners/pages/classes.blade.php
+++ resources/views/learners/pages/classes.blade.php
@@ -226,7 +226,7 @@
               <!-- Add to Playlist Modal -->
              
               
-              @if($filtres_applied)
+            @if($filtres_applied)
                   <div class="la-courses__content-desc">
                     <div class="la-anim__wrap">
                       <div class="row row-cols-md-2 row-cols-lg-3 row-cols-xl-4 la-anim__stagger-item">
@@ -270,7 +270,7 @@
                         </div>
                         
                       @endif
-                      @else
+        	@else
                         <div class="tab-content la-courses__nav-content la-anim__wrap position-relative" id="nav-tabContent">
                               @foreach ($categories as $category)
                               <div class="tab-pane fade show la-anim__wrap @if ($loop->first) active @endif" id="nav-{{$category->slug}}" role="tabpanel" aria-labelledby="nav-{{$category->slug}}-tab">
@@ -292,7 +292,7 @@
                                                     $courses = $category->courses;     
                                                   @endphp
 
-                                                  @foreach($courses->where('featured','like','1') as $course)
+                                                  @foreach($courses->where('featured','like','1')->sortBy('order') as $course)
                                                    
                                                     
                                                     <div class="swiper-slide la-courses__featured-slide la-anim__stagger-item">
@@ -387,7 +387,7 @@
                               @endforeach
                         </div>
                   </div>        
-                 @endif
+            @endif
             
           </div>
         </div>
