bbf760947c8ab971c806b964a8dc04a878efd2e8
Harish Kumar <harishcanudo@gmail.com>
2021-05-20 11:12:17 +0530

    Tagmanager

--- resources/views/learners/layouts/app.blade.php
+++ resources/views/learners/layouts/app.blade.php
@@ -54,7 +54,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5CW2KJ5');</script>
+    })(window,document,'script','dataLayer','GTM-WPPRZG6');</script>
     <!-- End Google Tag Manager -->
 
     <!-- Facebook Pixel Code -->
@@ -78,7 +78,11 @@
 </head>
 
 <body>
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WPPRZG6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     @yield('body_scripts')
 
     <div class="la-app__inner">
