638d723c566154fe692e879f0451aa58650bbc62
officialvikramsuthar <73328353+officialvikramsuthar@users.noreply.github.com>
2021-05-25 08:41:40 +0530

    Update UserDashboardController.php

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -17,12 +17,12 @@ class UserDashboardController extends Controller
         $userInterest = UserInterest::where(['user_id'=>Auth::User()->id])->pluck('category_id');
         $lastViewed = UserWatchTimelog::with('course', 'course.courseclass','course.user')->where(['user_id'=>Auth::User()->id])->latest()->first();
         $pendingCourse= UserWatchProgress::with('courses','user')->where(['user_id'=>Auth::User()->id])->latest()->groupBy('course_id')->limit(4)->get();
-        
+
         $recentWatchedCourseCompletion = 0;
         if($lastViewed){
             //Total Class Videos
             $lastViewedTotalVideo = $lastViewed->course->courseclass->count();
-            
+
             // User watched videos
             $lastWatchedCourse = UserWatchProgress::where([ 'course_id'=> $lastViewed->course_id, 'user_id'=>Auth::User()->id ])->get();
             $lastWatchedCourseAvg = $lastWatchedCourse->avg('current_position'); // 60% competed
@@ -32,13 +32,13 @@ class UserDashboardController extends Controller
             $recentWatchedCourseCompletion = round(abs($lastWatchedCourseAvg / (abs($lastViewedTotalVideo - $lastWatchedCourseCount) + 1)));
         }
 
-        if($userInterest){
+        if(count($userInterest)){
             $courses = Course::with('user','category')->where(['status'=>1])->whereIn('category_id', $userInterest)->get();
-            
+
         }else{
             $courses = Course::with('user','category')->where(['status'=>1])->get();
         }
-       
+
         return view('learners.pages.user-dashboard', compact('courses','lastViewed', 'recentWatchedCourseCompletion','pendingCourse'));
     }
 }
