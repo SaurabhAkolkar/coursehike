8f8668456672f72f70e8e139b4fafb198cd69949
Anvesh Nandagiri <49784558+Anvesh0952@users.noreply.github.com>
2021-06-02 21:57:01 +0530

    Added CTA in new releases comp

--- resources/views/components/new-event.blade.php
+++ resources/views/components/new-event.blade.php
@@ -1,13 +1,16 @@
 <li class="la-news__meet-item la-anim__wrap">
-    <h6 class="la-news__meet-title head-font text-lg text-md-xl m-0 text-capitalize la-anim__stagger-item--x"> {{ $title }} </h6>
-    <p class="text-sm la-news__meet-timestamp la-anim__stagger-item--x"> {{ $timestamp }}</p>
+    <h6 class="la-news__meet-title head-font text-lg text-md-xl m-0 text-capitalize la-anim__stagger-item"> {{ $title }} </h6>
+    <p class="text-sm la-news__meet-timestamp la-anim__stagger-item"> {{ $timestamp }}</p>
 
-    <div class="la-news__meet-content">
-        <p class="text-md la-anim__stagger-item--x"> {{ $about }} </p>
-        <div class="la-news__meet-banner la-anim__stagger-item">
+    <div class="la-news__meet-content la-anim__stagger-item">
+        <p class="text-md"> {{ $about }} </p>
+        <a  href="" role="button" class="la-news__meet-banner ">
           <img class="d-block lazy" src= "{{ $img }}" data-src= "{{ $img }}" alt="{{ $title }}" />
-        </div>
-        <p class="text-md my-3 collapse la-anim__stagger-item" id= "event_{{ $eventId }}" > {{ $desc }} </p>
+        </a>
+        <p class="text-md my-3 collapse" id= "event_{{ $eventId }}" > 
+          <span>{{ $desc }}</span> <br/>
+          <a role="button" href="" class="btn btn-primary la-btn__app py-3  mt-8 mt-md-5">Explore</a>
+        </p>
     </div>
     <p class="la-news__readmore collapsed text-center text-sm-right mt-3 la-anim__stagger-item" role="button" href="#event_{{ $eventId }}" data-toggle="collapse" aria-expanded="true">Read More</p>
 </li>
--- resources/views/components/new-update.blade.php
+++ resources/views/components/new-update.blade.php
@@ -1,17 +1,22 @@
 <li class="la-news__item la-anim__wrap"> 
     <div class="la-news__wrapper d-flex flex-column flex-lg-row justify-content-between">
-        <div class="la-news__eimg la-anim__stagger-item">
+        <a class="la-news__eimg la-anim__stagger-item" href="">
             <img class="d-block lazy" src= "{{ $img }}" data-src= "{{ $img }}" alt= "{{ $title }}" />
-        </div>
+        </a>
 
         <div class="la-news__etitle">
             <h6 class="la-news__title text-lg text-md-xl head-font m-0 la-anim__stagger-item--x"> {{ $title }} </h6>
             <span class="la-news__timestamp text-sm la-anim__stagger-item--x"> {{ $timestamp }} </span>
             <div class="la-news__content text-md text-sm-sm mb-3">
                 <span class="la-anim__stagger-item--x"> {{ $desc }}  </span>
-                <span class="collapse" id= "update_{{ $updateId }}"> {{ $desc }}  </span>
+                <span class="collapse" id= "update_{{ $updateId }}"> 
+                    <span>{{ $desc }}  </span> <br/>
+                    <a role="button" href="" class="btn btn-primary la-btn__app py-3 mt-8 mt-md-5">Explore</a>
+                </span>
             </div>
             <p class="la-news__readmore collapsed text-center text-sm-right la-anim__stagger-item--x" role="button" href="#update_{{ $updateId }}" data-toggle="collapse" aria-expanded="true">Read More</p>
+        
+           
         </div>
     </div>
 </li>
