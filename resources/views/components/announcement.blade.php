82c5f61abe942b3bfdf527a548aa28afffd9d5cd
Anvesh Nandagiri <49784558+Anvesh0952@users.noreply.github.com>
2021-06-11 23:24:48 +0530

    UI changes in announcements

--- resources/views/components/announcement.blade.php
+++ resources/views/components/announcement.blade.php
@@ -1,13 +1,18 @@
-<li class="la-announcement__item">
-    <a class="la-announcement__link" href="/releases/{{ $url }}">
-        <div class="la-announcement__link-inner d-flex flex-row align-items-center">
+<div class="la-announcement__item">
+    <a class="la-announcement__link d-md-flex align-items-center justify-content-between" href="/releases/{{ $url }}">
+        <div class="col-md-10 px-0 la-announcement__link-inner d-flex flex-row align-items-center">
             <div class="la-announcement__eprfle">
                 <img class="d-block lazy" src= "{{ $img }}" data-src= "{{ $img }}" alt= "{{ $event }}" />
             </div>
-             <div class="la-announcement__event text-sm ml-3 pr-2">{{ strlen($event)>30?substr($event, 0, 30).'....':$event }}</div>
+             <div class="la-announcement__info ml-3 pr-2">
+                 <div class="la-announcement__info-tag">Bundle Course</div>
+                 <p class="la-announcement__info-event leading-tight mb-1">{{ strlen($event)>30?substr($event, 0, 30).'....':$event }}</p>
+                 <p class="la-announcement__info-name mb-1">Sunny Bhanushali</p>
+                 <div class="la-announcement__info-duration">2h 15m</div>
+            </div>
         </div>
-        <div class="la-announcement__timestamp text-right text-xs font-normal"> {{ $timestamp }} </div>
+        <div class="col-md-2 px-0 la-announcement__timestamp text-right"> {{ $timestamp }} </div>
     </a>
-</li>
+</div>
 
 
